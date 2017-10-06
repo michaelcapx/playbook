5b14a390ac9595fcf1341cfbe7fc948cd5e54518
Michael Cap <michaelcapx@gmail.com>
2017-10-06 21:47:06 +0000

    Add main lamp tasks

--- docs/lamp.sh
+++ docs/lamp.sh
@@ -47,6 +47,27 @@ update_system() {
 install_apache() {
   e_header "Installing Apache......."
   sudo apt-get install -y apache2
+
+  # Set global ServerName
+  echo 'ServerName localhost' | sudo tee --append /etc/apache2/apache2.conf > /dev/null
+  sudo apache2ctl configtest
+
+  # Configure MPM Prefork
+  sudo sed -i "s/StartServers\s\{2,\}[0-9]*/StartServers       4/" /etc/apache2/mods-available/mpm_prefork.conf
+  sudo sed -i "s/MinSpareServers\s\{2,\}[0-9]*/MinSpareServers     3/" /etc/apache2/mods-available/mpm_prefork.conf
+  sudo sed -i "s/MaxSpareServers\s\{2,\}[0-9]*/MaxSpareServers    40/" /etc/apache2/mods-available/mpm_prefork.conf
+  sudo sed -i "s/MaxRequestWorkers\s\{2,\}[0-9]*/MaxRequestWorkers   200/" /etc/apache2/mods-available/mpm_prefork.conf
+  sudo sed -i "s/MaxConnectionsPerChild\s\{2,\}[0-9]*/MaxConnectionsPerChild   10000/" /etc/apache2/mods-available/mpm_prefork.conf
+
+  # Disable the event module and enable prefork
+  sudo a2dismod mpm_event
+  sudo a2enmod mpm_prefork
+
+  # Enable Apache mod_rewrite
+  sudo a2enmod rewrite
+
+  # Restart Apache
+  sudo systemctl restart apache2
 }
 
 ###########################################################################
@@ -55,6 +76,29 @@ install_apache() {
 
 install_mysql() {
   e_header "Installing MySQL......."
+
+  # Install MySQL
+  sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password secret'
+  sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password secret'
+  sudo apt-get install -y mysql-server
+
+  # Configure MySQL Password Lifetime
+  echo 'default_password_lifetime = 0' | sudo tee --append /etc/mysql/mysql.conf.d/mysqld.cnf > /dev/null
+
+  # Setup root MySQL user
+  mysql --user="root" --password="secret" -e "GRANT ALL ON *.* TO root@localhost IDENTIFIED BY 'secret' WITH GRANT OPTION;"
+  sudo service mysql restart
+
+  # Create main MySQL user
+  mysql --user="root" --password="secret" -e "CREATE USER 'homestead'@'localhost' IDENTIFIED BY 'secret';"
+  mysql --user="root" --password="secret" -e "GRANT ALL ON *.* TO 'homestead'@'localhost' IDENTIFIED BY 'secret' WITH GRANT OPTION;"
+  mysql --user="root" --password="secret" -e "GRANT ALL ON *.* TO 'homestead'@'%' IDENTIFIED BY 'secret' WITH GRANT OPTION;"
+  mysql --user="root" --password="secret" -e "FLUSH PRIVILEGES;"
+  mysql --user="root" --password="secret" -e "CREATE DATABASE homestead character set UTF8mb4 collate utf8mb4_bin;"
+  sudo service mysql restart
+
+  # Add Timezone Support To MySQL
+  # mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql --user=root --password=secret mysql
 }
 
 ###########################################################################
@@ -64,11 +108,53 @@ install_mysql() {
 install_php() {
   e_header "Installing PHP......."
 
+  # Add PHP PPA
   if ! grep -q "ondrej/php" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
     e_header "Adding PHP PPA......."
     sudo apt-add-repository -y ppa:ondrej/php
     sudo apt-get update
   fi
+
+  php_pkgs=(
+    php7.1
+    libapache2-mod-php7.1
+    # php7.1-apcu
+    php7.1-bcmath
+    php7.1-bz2
+    php7.1-cgi
+    php7.1-cli
+    # php7.1-common
+    php7.1-curl
+    php7.1-dev
+    # php7.1-fpm
+    php7.1-gd
+    php7.1-intl
+    php7.1-imagick
+    php7.1-imap
+    php7.1-json
+    php7.1-ldap
+    php7.1-mbstring
+    php7.1-mcrypt
+    php7.1-memcached
+    # php7.1-mongodb
+    php7.1-mysql
+    # php7.1-opcache
+    php7.1-pgsql
+    php7.1-readline
+    # php7.1-redis
+    php7.1-soap
+    php7.1-sqlite3
+    # php7.1-xdebug
+    php7.1-xml
+    php7.1-xmlrpc
+    php7.1-yaml
+    php7.1-zip
+    php-xdebug
+    php-pear
+  )
+
+  # Install PHP Packages
+  sudo apt-get install -y --allow-downgrades --allow-remove-essential --allow-change-held-packages "${php_pkgs[@]}"
 }
 
 ###########################################################################
